d3800b4a1744507287ef31efb9272e86dc1b2bc4
Dennis Fedorov <115027699+DennisUser1@users.noreply.github.com>
2024-05-21 21:38:48 +0300

    basic functionality

---
+++ scripts/functions.js
@@ -0,0 +1,61 @@
+import refs from "./refs.js";
+import { load, save } from "./storage.js";
+
+const STORAGE_KEY = "tasks";
+let currentID = 1;
+
+function addCloseButton(target) {
+    const span = document.createElement("span");
+    const txt = document.createTextNode("\u00D7");
+    span.className = "close";
+    span.appendChild(txt);
+    target.appendChild(span);
+  }
+
+function addNewTask() {
+    const clearInput = () => (refs.myInput.value = "");
+
+    const value = refs.myInput.value.trim();
+    if (value === "") {
+      alert("Потрібно ввести текст!");
+      clearInput();
+      return;
+    }
+    createLi({
+      text: value,
+    });
+  
+    addTaskToStorage(value);
+    clearInput();
+  }
+
+  function createLi({ text, isDone = false, id = currentID }) {
+    const liEl = document.createElement("li");
+    liEl.textContent = text;
+    liEl.dataset.id = id;
+    if (isDone) liEl.className = "checked";
+    addCloseButton(liEl);
+    refs.myUL.appendChild(liEl);
+  }
+
+  function handleTaskBehaviour({ target }) {
+    const currentState = load(STORAGE_KEY);
+  
+    if (target.nodeName === "LI") {
+      target.classList.toggle("checked");
+      const taskObj = currentState.find(
+        (task) => Number(task.id) === Number(target.dataset.id)
+      );
+      taskObj.isDone = !taskObj.isDone;
+    } else if (target.classList.contains("close")) {
+      target.parentNode.remove();
+      const taskIndex = currentState.findIndex(
+        (task) => Number(task.id) === Number(target.parentNode.dataset.id)
+      );
+      currentState.splice(taskIndex, 1);
+    }
+  
+    save(STORAGE_KEY, currentState);
+  }
+
+export { addNewTask, handleTaskBehaviour, fillTasksList };
